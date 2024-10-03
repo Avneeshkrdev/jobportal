1150b7e364def46aa881cb6bf0640a80bf3a07df
Avneesh <avneeshkrdev@gmail.com>
2024-10-03 20:00:42 +0530

    trial2

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin: "https://jobportal-ecru.vercel.app/", credentials: true}));
+app.use(cors({origin: "https://jobportal-ecru.vercel.app", credentials: true}));
 
 const PORT = process.env.PORT || 3000;
 
