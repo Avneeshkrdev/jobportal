adbe0f8e370d0f895cebd3dd67e7ebd74af8260c
Avneesh <avneeshkrdev@gmail.com>
2024-10-26 11:02:36 +0530

    new again

--- frontend/src/components/admin/AdminJobs.jsx
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux'
 import AdminJobsTable from './AdminJobsTable'
 import useGetAllAdminJobs from '@/hooks/useGetAllAdminJobs'
 import { setSearchJobByText } from '@/redux/jobSlice'
+import { ArrowLeft } from 'lucide-react'
 
 const AdminJobs = () => {
   useGetAllAdminJobs();
@@ -23,8 +24,16 @@ const AdminJobs = () => {
       <Navbar />
       <div className=' mx-auto my-10 px-4 sm:px-6 lg:px-8'>
         <div className='flex flex-col sm:flex-row items-center justify-between my-5'>
+        <Button
+                      onClick={() => navigate("/admin/companies")}
+                            variant="outline"
+                            className="flex items-center gap-4 justify-center  text-gray-500 font-semibold"
+                        >
+                            <ArrowLeft className='w-4 h-4' />
+                            <span>Back</span>
+                    </Button>
           <Input
-            className="w-full sm:w-1/2 mb-4 sm:mb-0"
+            className="w-1/2 mb-4 sm:mb-0"
             placeholder="Filter by name, role"
             onChange={(e) => setInput(e.target.value)}
           />
